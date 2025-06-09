7104e8b11a11a59563a6c5e0e3f6329aa5abd506
BoyTipak <132035126+kopalKaba@users.noreply.github.com>
2025-06-09 12:41:38 +0800

    Update etc.js

--- chains/utils/etc.js
+++ chains/utils/etc.js
@@ -7,18 +7,11 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 function header() {
   process.stdout.write('\x1Bc');
-  console.log('===================================================='.cyan);
-  console.log('                                                    '.cyan);
-  console.log(' 8888888b.  d8b                        888          '.cyan);
-  console.log(' 888   Y88b Y8P                        888          '.cyan);
-  console.log(' 888    888                            888          '.cyan);
-  console.log(' 888   d88P 888  8888b.  88888b.   .d88888  8888b.  '.cyan);
-  console.log(' 8888888P"  888     "88b 888 "88b d88" 888     "88b '.cyan);
-  console.log(' 888 T88b   888 .d888888 888  888 888  888 .d888888 '.cyan);
-  console.log(' 888  T88b  888 888  888 888  888 Y88b 888 888  888 '.cyan);
-  console.log(' 888   T88b 888 "Y888888 888  888  "Y88888 "Y888888 '.cyan);
-  console.log('                                                    '.cyan);
-  console.log('===================================================='.cyan);
+  console.log('=============================='.cyan);
+  console.log('                              '.cyan);
+  console.log('        KOPAL KABA            '.cyan);
+  console.log('                              '.cyan);
+  console.log('=============================='.cyan);
   console.log();
 }
 
